da0801f64255a9d9a36ccad17761ef27f48c6992
jionghao Wu <jionghaowu@jionghaos-MacBook-Pro.local>
2024-09-29 22:10:56 -0400

    first commit

---
+++ gp_apis.py
@@ -0,0 +1,11 @@
+import torch as th
+import torch.utils.dlpack
+import graphpy as gpk
+
+def gp_MatrixMuliplication(input1, input2, dim1_0, dim1_1, device0):
+    input1_dl = th.utils.dlpack.to_dlpack(input1)
+    input2_dl = th.utils.dlpack.to_dlpack(input2)
+    res1 = th.zeros(dim1_0, dim1_1, device = device0)
+    res_dl1 = th.utils.dlpack.to_dlpack(res1)
+    gpk.MatrixMuliplication(input1_dl, input2_dl, res_dl1)
+    return res1
